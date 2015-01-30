3d92091b071a025bc324e1fb50424a3f35f0a802
Seth Tippetts <sethtippetts@gmail.com>
2015-01-29 17:07:04 -0700

    Added new grunt config for local SDK build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,28 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')        
+        pkg: grunt.file.readJSON('package.json'),
+        abbr: __dirname.split('/').pop(),
+        sdk: {
+            options: {
+                modules: [
+	               	'AdminApp',
+
+	               	// Module inclusion
+	               	'AppModule',
+	               	'InterceptorModule',
+	               	'EntertainerModule',
+	               	'MessageModule',
+	               	'SubscriberModule',
+	               	'SiteModule',
+	               	'UserModule',
+	               	'EventModule',
+	               	'VenueModule',
+	               	'MediaModule',
+	               	'PageModule'
+                ]
+            }
+        }   
     });
 
     grunt.loadTasks('../global/grunt');
