050a97874f98d5e8996fcf14da3ef0670f59c63a
Seth Tippetts <sethtippetts@gmail.com>
2015-02-17 08:09:02 -0700

    Added showtime object and entertainer typeahead (not working)

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
             		'subscriber',
             		'user',
             		'event',
+                    'showtime',
             		'venue',
             		'medium',
             		'section',
