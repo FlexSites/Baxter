f9a82a5a98db30ac64083ab2e18c7d63394d007e
Seth Tippetts <sethtippetts@gmail.com>
2015-03-01 18:58:18 -0700

    Updated grunt build path

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        abbr: __dirname.split('/').pop(),
-        render: {
-            options: {
-            	resources: [
-                    'site',
-            		'entertainer',
-            		'subscriber',
-            		'user',
-            		'event',
-                    'showtime',
-            		'venue',
-            		'medium',
-            		'section',
-            		'page',
-            		'message'
-            	]
-            }
-        }   
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    abbr: __dirname.split('/').pop(),
+    render: {
+      options: {
+        resources: [
+          'site',
+          'entertainer',
+          'subscriber',
+          'user',
+          'event',
+          'showtime',
+          'venue',
+          'medium',
+          'section',
+          'page',
+          'message'
+        ]
+      }
+    }
+  });
 
-    grunt.loadTasks('../global/grunt');
+  grunt.loadTasks('/www/auto/grunt');
 };
