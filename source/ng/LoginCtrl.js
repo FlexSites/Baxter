2e210048dfe21ff2664d6a8c3fb0a968ea7b165f
Seth Tippetts <sethtippetts@gmail.com>
2015-03-30 23:42:40 -0600

    Adding `display: none` to splash until we can fix it. Fixing $rootScope.user bug

--- source/ng/LoginCtrl.js
+++ source/ng/LoginCtrl.js
@@ -1,11 +1,10 @@
 angular.module('app')
   .controller('LoginCtrl', ['$window', '$scope', '$state', '$rootScope', 'User' ,function($window, $scope, $state, $rootScope, User ){
-    $scope.user = new User();
+    $rootScope.user = new User();
     $scope.login = function(){
-      $scope.user.$login(function(user){
+      $rootScope.user.$login(function(user){
         $state.go($window.sessionStorage.returnTo || 'home');
         delete $window.sessionStorage.returnTo;
       });
     };
-    $rootScope.user = user;
   }]);
